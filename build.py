84b606bbf84b31e0b254846fbbc9c1ba45588729
Marcin Lulek <info@webreactor.eu>
2018-10-27 11:53:57 +0200

    initial commit

---
+++ build.py
@@ -0,0 +1,200 @@
+import string
+import pathlib
+import markdown
+import time
+import logging
+import os
+
+from shutil import copy2, copystat, Error
+
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+logging.basicConfig(level=logging.WARNING)
+
+log = logging.getLogger(__name__)
+
+
+class Config:
+    def __init__(self, template_pkg, index_path):
+        self.template_pkg = template_pkg
+        self.template_path = pathlib.Path(template_pkg).absolute()
+        self.index_path = index_path
+        self.assets_path = index_path.parent / "assets"
+        self.output_path = pathlib.Path("out").absolute()
+
+
+class Slide:
+    def __init__(self, markdown_text, index):
+        self.reveal_md = True
+        md = markdown.Markdown(
+            extensions=[
+                "markdown.extensions.abbr",
+                "markdown.extensions.attr_list",
+                "markdown.extensions.def_list",
+                "markdown.extensions.footnotes",
+                "markdown.extensions.tables",
+                "markdown.extensions.admonition",
+                "markdown.extensions.codehilite",
+                "markdown.extensions.meta",
+                "markdown.extensions.attr_list",
+            ]
+        )
+        html = md.convert(markdown_text.strip())
+        self.meta = md.Meta
+        self.markdown = markdown_text
+        self.html = html
+        self.index = index
+
+    def render(self):
+        class_ = f"slide slide-{self.index}"
+        meta_pairs = []
+        for k, v in self.meta.items():
+            if k.startswith("data-"):
+                meta_pairs.append(f'{k}="{v[0]}"')
+        meta = " ".join(meta_pairs)
+        if self.reveal_md:
+            return f"""
+            <section data-markdown>
+            <textarea data-template>
+            {self.markdown}
+            </textarea>
+            </section>"""
+        return f'<section class="{class_}" {meta}>{self.html}</section>'
+
+    @property
+    def rendered(self):
+        return self.render()
+
+
+# copied from stdlib and patched to not error out on existing directories
+def copytree(
+    src,
+    dst,
+    symlinks=False,
+    ignore=None,
+    copy_function=copy2,
+    ignore_dangling_symlinks=False,
+):
+
+    names = os.listdir(src)
+    if ignore is not None:
+        ignored_names = ignore(src, names)
+    else:
+        ignored_names = set()
+    # do not fail on existing dirs
+    os.makedirs(dst, exist_ok=True)
+    errors = []
+    for name in names:
+        if name in ignored_names:
+            continue
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                if symlinks:
+                    # We can't just leave it to `copy_function` because legacy
+                    # code with a custom `copy_function` may rely on copytree
+                    # doing the right thing.
+                    os.symlink(linkto, dstname)
+                    copystat(srcname, dstname, follow_symlinks=not symlinks)
+                else:
+                    # ignore dangling symlink if the flag is on
+                    if not os.path.exists(linkto) and ignore_dangling_symlinks:
+                        continue
+                    # otherwise let the copy occurs. copy2 will raise an error
+                    if os.path.isdir(srcname):
+                        copytree(srcname, dstname, symlinks, ignore, copy_function)
+                    else:
+                        copy_function(srcname, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks, ignore, copy_function)
+            else:
+                # Will raise a SpecialFileError for unsupported file types
+                copy_function(srcname, dstname)
+        # catch the Error from the recursive copytree so that we can
+        # continue with other files
+        except Error as err:
+            errors.extend(err.args[0])
+        except OSError as why:
+            errors.append((srcname, dstname, str(why)))
+    try:
+        copystat(src, dst)
+    except OSError as why:
+        # Copying file access times may fail on Windows
+        if getattr(why, "winerror", None) is None:
+            errors.append((src, dst, str(why)))
+    if errors:
+        raise Error(errors)
+    return dst
+
+
+def grab_slides(config):
+    presentation_chunks = config.index_path.read_text("utf8").split("---------")
+    return [
+        Slide(markdown_text=slide_md, index=ix)
+        for ix, slide_md in enumerate(presentation_chunks, 1)
+    ]
+
+
+def generate_presentation(config):
+    print("Generating presentation")
+    template = string.Template(
+        (config.template_path / "template.html").read_text("utf8")
+    )
+    slides = grab_slides(config)
+    sections = [s.rendered for s in slides]
+    rendered_template = template.safe_substitute(slides="\n".join(sections))
+
+    def copy_ignore(dir_, filelist):
+        if config.template_path == pathlib.Path(dir_):
+            return ["template.html"]
+        return []
+
+    config.output_path.mkdir(exist_ok=True)
+    copytree(config.template_path, config.output_path, ignore=copy_ignore)
+    print("Templating...")
+    (config.output_path / "index.html").write_text(rendered_template, "utf8")
+    if config.assets_path.exists():
+        print(f"Copying {config.assets_path} to output dir")
+        copytree(config.assets_path, config.output_path / "assets")
+    else:
+        print(f"Assets dir not found in {config.assets_path}")
+
+    print("...Done...")
+
+
+class PotatoStampHandler(LoggingEventHandler):
+    def __init__(self, config):
+        self.config = config
+
+    def on_modified(self, event):
+        super().on_modified(event)
+        what = "directory" if event.is_directory else "file"
+
+        index_changed = (
+            not event.is_directory
+            and self.config.index_path == pathlib.Path(event.src_path)
+        )
+        assets_changed = event.is_directory and self.config.assets_path == pathlib.Path(
+            event.src_path
+        )
+        if index_changed or assets_changed:
+            generate_presentation(self.config)
+
+
+if __name__ == "__main__":
+    index_path = pathlib.Path("open_source/index.md").absolute()
+    config = Config(template_pkg="revealjs_template", index_path=index_path)
+    generate_presentation(config)
+    event_handler = PotatoStampHandler(config)
+    observer = Observer()
+    observer.schedule(event_handler, str(config.index_path.parent), recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
---
+++ open_source/index.md
@@ -0,0 +1,159 @@
+
+# Open Source
+
+Marcin Lulek
+
+Senior Software Engineer @ P2Sample
+
+https://github.com/ergo, info@webreactor.eu
+
+![P2Sample Logo](assets/img/logo.png)
+
+---------
+
+## P2Sample
+
+* Badania rynku oraz targetowane panele badawcze
+* Machine Learning i Fraud detection
+* 50M+ użytkowników
+* Baza danych o wielkości ponad 1TB
+* Niektóre tabele przekraczają 1,000,000,000 wierszy.
+
+![logo](assets/img/klienci/gfk.png)
+![logo](assets/img/klienci/mwb.png)
+![logo](assets/img/klienci/ipsos.png)
+
+---------
+
+### Czego używamy
+
+* Pyramid i Pylons
+* SQLAlchemy
+* Celery
+* MySQL :(
+* Elasticsearch
+* Apache AirFlow
+* Ansible
+* Cython
+
+---------
+
+### Python AD 2009 
+
+* Django pomiędziy 0.96 a 1.0 alpha
+* Pylons 0.9.7
+* SQLAlchemy 0.5 
+* Celery 0.8
+
+---------
+
+#### Projekty
+
+* AppEnlight (https://github.com/appenlight/)
+* Channelstream (https://github.com/Channelstream/channelstream)
+* Projekty Pyramid (https://github.com/pylons)
+* RhodeCode (https://rhodecode.com/)
+* Mozilla Cornice (https://github.com/Cornices/cornice)
+* Webhelpers Grid (https://github.com/ergo/webhelpers2_grid)
+* Ziggurat Foundations (https://github.com/ergo/ziggurat_foundations)
+
+---------
+
+### Czemu warto współpracować przy Otwartym Oprogramowaniu
+
+* Doświadczenie w pracy w grupie
+* Nauka przez czytanie kodu i review przez doświadczone osoby <!-- .element: class="fragment" -->
+* Budowa własnego portfolio <!-- .element: class="fragment" -->
+* Rozwój projektów potrzebnych w pracy <!-- .element: class="fragment" -->
+* Możliwości zarobkowe <!-- .element: class="fragment" -->
+* Zwiedzisz świat jako prelegent na konferencjach IT <!-- .element: class="fragment" -->
+
+---------
+
+### Czemu nie warto pracować przy Otwartym Oprogramowaniu
+
+* Brak możliwości zarobkowych ;)
+* Pochłania duże nakłady czasu  <!-- .element: class="fragment" -->
+* "Trudni klienci"  <!-- .element: class="fragment" -->
+* Toksyczne społeczności  <!-- .element: class="fragment" -->
+
+<video data-autoplay src="assets/videos/dog.mp4" style="max-height:300px"></video>
+
+---------
+
+### Jak zacząć
+
+Rodzaje wsparcia dla projektów:
+
+* Zgłaszanie błędów
+* Dokumentacja  <!-- .element: class="fragment" -->
+* Moderacja treści i wsparcie na forach  <!-- .element: class="fragment" -->
+* Tworzenie nowych funkcjonalności  <!-- .element: class="fragment" -->
+* Planowanie rozwoju  <!-- .element: class="fragment" -->
+
+---------
+
+* https://github.com/explore/ lub https://up-for-grabs.net
+* label:"good first issue" oraz  label:"help wanted"
+* https://www.codetriage.com/?language=Python 
+* Pamiętajmy o przeczytaniu licencji oraz contributing guidelines
+* Zainstaluj projekt lokalnie
+* Jeśli projekt ma chat/kanał ircowy warto tam zajrzeć
+
+---------
+
+### Prawo
+
+<!-- .slide: data-background-image="assets/img/hammer-620009_1280-cc0.jpg" -->
+
+---------
+
+### Licencje
+
+Rodzaje licencji i co się z nimi wiąże.
+
+Najpopularniejsze to: MIT, BSD, Apache 2.0, GPL, AGPL.
+
+Porównywarki licencji:
+
+* https://choosealicense.com/
+* https://tldrlegal.com/
+
+---------
+
+* Licencje mogą działać wirusowo 
+* Nie każda licencja się łączy z inną licencją open source  <!-- .element: class="fragment"  data-fragment-index="1" -->
+* Uwaga na szerokie klauzule patentowe (starszy React i licencje FB)  <!-- .element: class="fragment"  data-fragment-index="2" -->
+* Nie wymyślaj własnych licencji  <!-- .element: class="fragment"  data-fragment-index="3" -->
+* Nie dyskryminuj ludzi ze względu na przekonania <!-- .element: class="fragment"  data-fragment-index="4" --> (casus Lerna http://esr.ibiblio.org/?p=8106) <!-- .element: class="fragment"  data-fragment-index="4" -->
+* Zmiana licencji wymaga ważnego prawnie potwierdzenia współtwórcy oprogramowania  <!-- .element: class="fragment"  data-fragment-index="5" -->
+
+
+---------
+
+### Prawo - Contributor License Agreements
+
+Przykłady projektów/organizacji które wymagają CLA:
+
+* Google
+* Apache Software Foundation
+* Canonical Ltd
+* Django
+* Python
+
+Generator CLA: http://www.harmonyagreements.org
+
+---------
+
+### Twój projekt
+
+* Dokumentacja jest ważna
+* Setup.py i pakowanie  <!-- .element: class="fragment"  data-fragment-index="2" --> (https://github.com/pypa/sampleproject) <!-- .element: class="fragment"  data-fragment-index="2" -->
+* Repozytorium kodu !== katalog pracy aplikacji  <!-- .element: class="fragment"  data-fragment-index="3" --> 
+* Testy <!-- .element: class="fragment"  data-fragment-index="4" --> (https://docs.pytest.org)  <!-- .element: class="fragment"  data-fragment-index="4" --> <video data-autoplay src="assets/videos/explosion_cat.mp4" style="max-height:300px"></video> <!-- .element: class="fragment"  data-fragment-index="4" -->
+* Wydziel publiczne i prywatne API.  <!-- .element: class="fragment"  data-fragment-index="5" --> 
+
+
+---------
+
+### Create Open Source and prosper!
